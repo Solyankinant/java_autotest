d02f7c0087aa54ec583604cab5c0cb16014dcb13
Anton <revizor2626@yandex.ru>
2018-07-12 13:53:01 +0300

    Добавлен новый проект hometest

---
+++ hometest/src/test/java/ru/solyankinas/autotest/HomeTest.java
@@ -0,0 +1,38 @@
+package ru.solyankinas.autotest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class HomeTest {
+
+    private static WebDriver driver;
+
+    @BeforeTest
+    public static void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/AutoTest/first-test/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://yandex.ru");
+    }
+
+    @Test
+    public void fieldSeach() {
+        String textSearch = "Погода Пенза";
+        driver.findElement(By.cssSelector(("#text"))).sendKeys(textSearch);
+        driver.findElement(By.cssSelector(".search2__button button")).click();
+        String textFirstLink = driver.findElement(By.cssSelector("[href='https://yandex.ru/pogoda/penza?from=serp_title']")).getText();
+        String textComparison = "Погода";
+        Assert.assertTrue(textFirstLink.contains(textComparison));
+    }
+
+    @AfterTest
+    public static void exitBrowser() {
+        driver.quit();
+    }
+}
