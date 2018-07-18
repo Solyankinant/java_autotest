efab76797cfcc3c9df02e8de17e8b67b34f55457
Anton <revizor2626@yandex.ru>
2018-07-18 14:15:47 +0300

    исправил метод поиска по тексту и перенес все методы в  BaseTest

--- hometest/src/test/java/ru/solyankinas/autotest/BaseTest.java
+++ hometest/src/test/java/ru/solyankinas/autotest/BaseTest.java
@@ -1,5 +1,6 @@
 package ru.solyankinas.autotest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -22,5 +23,17 @@ public class BaseTest {
     @AfterTest
     public static void exitBrowser() {
         driver.quit();
+    }
+
+    protected String getTextFirstLink() {
+        return driver.findElement(By.cssSelector("a[accesskey='1']")).getText();
+    }
+
+    protected void clickButtonSearch() {
+        driver.findElement(By.cssSelector("form[role='search'] button[type='submit']")).click();
+    }
+
+    protected void inputTextSearch(String textSearch) {
+        driver.findElement(By.cssSelector(("#text"))).sendKeys(textSearch);
     }
 }
--- hometest/src/test/java/ru/solyankinas/autotest/HomeTest.java
+++ hometest/src/test/java/ru/solyankinas/autotest/HomeTest.java
@@ -1,6 +1,5 @@
 package ru.solyankinas.autotest;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,23 +8,10 @@ public class HomeTest extends BaseTest{
 
     @Test
     public void fieldSeach() {
-        textSearch();
+        inputTextSearch("Погода Пенза");
         clickButtonSearch();
         String textComparison = "Погода";
         Assert.assertTrue(getTextFirstLink().contains(textComparison));
-    }
-
-    private String getTextFirstLink() {
-        return driver.findElement(By.cssSelector("a[accesskey='1']")).getText();
-    }
-
-    private void clickButtonSearch() {
-        driver.findElement(By.cssSelector("form[role='search'] button[type='submit']")).click();
-    }
-
-    private void textSearch() {
-        String textSearch = "Погода Пенза";
-        driver.findElement(By.cssSelector(("#text"))).sendKeys(textSearch);
     }
 
 }
