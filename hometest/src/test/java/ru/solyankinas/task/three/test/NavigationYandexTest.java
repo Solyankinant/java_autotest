3e002f2c345a988e25140914c498085bbc369228
Anton <revizor2626@yandex.ru>
2018-07-31 16:35:35 +0300

    Revert "Добавил файл с описаниет тестсьюта и исправил ПОМ"
    This reverts commit 5f38306fc662cc01a11f248a35861da6fb3b560e.

---
+++ hometest/src/test/java/ru/solyankinas/task/three/test/NavigationYandexTest.java
@@ -0,0 +1,56 @@
+package ru.solyankinas.task.three.test;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+public class NavigationYandexTest extends TestBase {
+    @Test
+    public void testNavigationYandex() {
+
+
+        SoftAssert asert = new SoftAssert();
+
+        getYandexNavigationPage().selectNavigation("Видео");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Свежее видео", getYandexNavigationPage().elementYandexVideo, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://yandex.ru/video/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Картинки");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Мои коллекции", getYandexNavigationPage().elementYandexImages, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://yandex.ru/images/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Новости");
+        asert.assertTrue(getYandexNavigationPage().waitLink("ПОЛИТИКА", getYandexNavigationPage().elementYandexNews,10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://news.yandex.ru/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Карты");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Поиск мест и адресов", getYandexNavigationPage().elementYandexMaps, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://yandex.ru/maps/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Маркет");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Компьютеры", getYandexNavigationPage().elementYandexMarket, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://market.yandex.ru/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Переводчик");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Переводчи", getYandexNavigationPage().elementYandexTranslate, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://translate.yandex.ru/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("Музыка");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Жанры", getYandexNavigationPage().elementYandexMusic, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://music.yandex.ru/"));
+
+        getYandexPage().openYandex();
+        getYandexNavigationPage().selectNavigation("ТВ онлайн");
+        asert.assertTrue(getYandexNavigationPage().waitLink("Что посмотреть", getYandexNavigationPage().elementYandexTv, 10));
+        asert.assertTrue(getYandexNavigationPage().compersionUrl("https://yandex.ru/1tv?stream_active=storefront"));
+
+        asert.assertAll();
+
+    }
+}
